582e3c8b0c41963036261f19d941a2b2e2d85ea5
cleitoncsl <cleitoncsl@gmail.com>
2023-10-12 20:34:12 -0300

    inicio 0.0.0.0.1

--- FlappyBird.py
+++ FlappyBird.py
@@ -19,5 +19,52 @@ FONTE_PONTOS = pygame.font.SysFont('arial', 50)
 
 
 
+class Passaro():
+    IMGS = IMAGEM_PASSARO
+    # animações da rotação
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    def __int__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.contagem = 0
+        self.imagem = IMGS[0]
+
+    def pular(self):
+        self.velocidade = -10.5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+    # calculo do deslocamento
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
+
+    # restringir o deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+
+    # angulo do passaro
+
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTACAO_MAXIMA:
+                self.angulo = self.ROTACAO_MAXIMA
 
 
+
+class Cano():
+    pass
+
+
+class Chao():
+    pass
